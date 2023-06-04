3516f79a3f115f5d83c4527be68ec258a2ac5bf8
DevChauhan5 <devchauhanthedev@gmail.com>
2023-06-04 14:23:52 +0530

    Basic UI

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import logo from "./../assets/Images/logo.png";
+import profile from "./../assets/Images/profile.png";
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+} from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
+import HeaderItem from "./HeaderItem.";
+
+function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  const menu = [
+    {
+      name: "HOME",
+      icon: HiHome,
+    },
+    {
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "WATCH LIST",
+      icon: HiPlus,
+    },
+    {
+      name: "ORIGINALS",
+      icon: HiStar,
+    },
+    {
+      name: "MOVIES",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "SERIES",
+      icon: HiTv,
+    },
+  ];
+  return (
+    <div className="flex items-center justify-between p-5">
+      <div className="flex gap-8 items-center">
+        <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
+        <div className="hidden md:flex gap-8">
+          {menu.map((item) => (
+            <HeaderItem name={item.name} Icon={item.icon} />
+          ))}
+        </div>
+        <div className="flex md:hidden gap-5">
+          {menu.map(
+            (item, index) =>
+              index < 3 && <HeaderItem name={""} Icon={item.icon} />
+          )}
+          <div className="md:hidden" onClick={() => setToggle(!toggle)}>
+            <HeaderItem name={""} Icon={HiDotsVertical} />
+            {toggle ? (
+              <div className="absolute mt-3 bg-[#121212] border-[1px] rounded-xl border-gray-700 p-3 px-5 py-4">
+                {menu.map(
+                  (item, index) =>
+                    index > 2 && (
+                      <HeaderItem name={item.name} Icon={item.icon} />
+                    )
+                )}
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+      <img src={profile} className="w-[40px] rounded-full" />
+    </div>
+  );
+}
+
+export default Navbar;

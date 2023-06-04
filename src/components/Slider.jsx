66cd0b6b4135661aa5448c9040f1c59162e04afc
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-06-04 11:40:21 +0000

    Update Slider.jsx

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from "../Services/GlobalApi"
+import { HiChevronLeft, HiChevronRight } from "react-icons/hi2"
 
+const screenWidth = window.innerWidth;
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 function Slider() {
   const [movieList, setMovieList] = useState([])
+  const elementRef = useRef();
   useEffect(() => {
     getTrendingMovies();
   }, [])
@@ -14,11 +17,30 @@ function Slider() {
     })
   }
 
+  const sliderRight = (element) => {
+    element.scrollLeft += screenWidth - 110
+  }
+  const sliderLeft = (element) => {
+    element.scrollLeft -= screenWidth - 110
+  }
+
   return (
-    <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none">
-      {movieList.map((item, index) => (
-        <img src={IMAGE_BASE_URL + item.backdrop_path} className="min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md" />
-      ))}
+    <div>
+      <HiChevronLeft
+        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer"
+        onClick={() => sliderLeft(elementRef.current)}
+      />
+      <HiChevronRight
+        className="hidden md:block text-white text-[30px] absolute mx-8 mt-[150px] cursor-pointer right-0"
+        onClick={() => sliderRight(elementRef.current)}
+      />
+      <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth" ref={elementRef}>
+        {movieList.map((item, index) => (
+          <img src={IMAGE_BASE_URL + item.backdrop_path}
+            className="min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in"
+          />
+        ))}
+      </div>
     </div>
   )
 }

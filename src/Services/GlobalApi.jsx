d8d6dd2b6de09996522495652e303fe7aac2d4ef
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-06-04 10:52:39 +0000

    Update 5 files

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const movieBaseUrl = "https://api.themoviedb.org/3";
 const api_key = import.meta.env.VITE_API_KEY;
 
-const getTrendingVideos = axios.get(this.movieBaseUrl + "/trending/all/day?api_key" + api_key);
+const getTrendingVideos = axios.get(movieBaseUrl + "/trending/all/day?api_key=" + api_key);
 export default {
     getTrendingVideos
 }
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import GlobalApi from "../Services/GlobalApi"
 
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
 function Slider() {
+  const [movieList, setMovieList] = useState([])
+  useEffect(() => {
+    getTrendingMovies();
+  }, [])
+
+  const getTrendingMovies = () => {
+    GlobalApi.getTrendingVideos.then(resp => {
+      setMovieList(resp.data.results);
+    })
+  }
+
   return (
-    <div>Slider</div>
+    <div className="flex overflow-x-auto w-full px-16 py-4 scrollbar-none">
+      {movieList.map((item, index) => (
+        <img src={IMAGE_BASE_URL + item.backdrop_path} className="min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md" />
+      ))}
+    </div>
   )
 }
 

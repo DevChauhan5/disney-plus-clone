406d94c9cb3a20bc24a2dbde970abb73f832cc9d
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-06-04 10:21:39 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Slider />
     </div>
   );
 }
